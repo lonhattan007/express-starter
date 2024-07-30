329c44af0a36c66b2c462e095d20242501402928
Ethan Lo <ethan.lo.int@shopback.com>
2024-07-30 15:45:55 +0700

    Update Jest configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,29 @@
-import type { Config } from "jest";
-import { defaults } from "jest-config";
+import { pathsToModuleNameMapper } from "ts-jest";
+import type { JestConfigWithTsJest } from "ts-jest";
+// import { compilerOptions } from "./tsconfig.json";
+import ts from "typescript";
 
-const config: Config = {
-  moduleFileExtensions: [...defaults.moduleFileExtensions, "test.ts"],
+const configFileName = ts.findConfigFile(
+  "./",
+  ts.sys.fileExists,
+  "tsconfig.json",
+);
+const configFile = ts.readConfigFile(configFileName!, ts.sys.readFile);
+const compilerOptions = ts.parseJsonConfigFileContent(
+  configFile.config,
+  ts.sys,
+  "./",
+);
+
+const jestConfig: JestConfigWithTsJest = {
+  preset: "ts-jest",
+  roots: ["<rootDir>"],
+  modulePaths: [compilerOptions.options.baseUrl!],
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.options.paths!, {
+      prefix: "<rootDir>",
+    }),
+  },
 };
 
-export default config;
+export default jestConfig;
--- tests/sample.test.ts
+++ tests/sample.test.ts
@@ -1,23 +1,22 @@
 import express from "express";
-import indexRouter from "../src/routes/index";
+import indexRouter from "@/routes/index";
 import { describe, it } from "@jest/globals";
-import request, { Response } from "supertest";
-import { HttpError } from "http-errors";
+import request from "supertest";
 
 describe("Sample test suite", () => {
   const app = express();
   app.use("/", indexRouter);
 
-  it("GET /", () => {
-    request(app)
+  it("GET /", (done) => {
+    const res = request(app)
       .get("/")
       .expect(302) // it is redirected to /bikes
-      .end(function (err: HttpError, res: Response) {
+      .end((err, res) => {
         if (err) {
-          throw err;
+          return done(err);
+        } else {
+          return done();
         }
-
-        console.info(res);
       });
   });
 });
